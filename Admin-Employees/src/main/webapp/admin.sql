165f85d894458066547d8e8d836c091c81ba5b78
Abdul Matin Walizada <walizadahabdulmatin@gmail.com>
2020-11-14 00:21:49 -0500

    sql script for project

---
+++ Admin-Employees/src/main/webapp/admin.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `admin_registeration` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_Name` varchar(45) DEFAULT NULL,
+  `last_Name` varchar(45) DEFAULT NULL,
+  `date_reg` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `user_Name` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
